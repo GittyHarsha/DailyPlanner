8767377e99851cd60f244f497614ad22319d5223
Harsha Narayana P <hnarayana788@gmail.com>
2024-03-30 20:41:24 +0530

    font styles

--- dailyplanner/src/components/AppBar.js
+++ dailyplanner/src/components/AppBar.js
@@ -25,23 +25,23 @@ function Header({name}) {
   }
   console.log("Inside Header Name: ", name)
   return (
-    <FlexDiv style={{marginLeft: `${w(23)}`, marginRight: `${w(23)}`}}>
+    <FlexDiv style={{marginLeft: `${w(23)}`, marginRight: `${w(13)}`, height: `${h(60)}`}}>
       
-        <FlexDiv  style={{color:'black',fontFamily: 'inter',fontSize: '1.5rem',fontWeight: 'bold', width: '45rem', height: '3.5rem',}}>
+        <FlexDiv  style={{color:'black',fontFamily: 'inter',fontSize: '1.8rem',fontWeight: 'bold', width: '45rem', height: '3.5rem',}}>
         
-        { (name)?(<>{Greeting()} {name} :)</> ):(<></>)}
+        { (name)?(<FlexDiv style={{height: '100%'}}>{Greeting()} {name} :)</FlexDiv> ):(<></>)}
           
         </FlexDiv>
        
 
-     <FlexDiv><SearchBar style={{width: '30.49421021377672rem'}}/>
+     <FlexDiv style={{height: '100%', width: '60%'}}><SearchBar style={{width: '70%', height: '50%'}}/>
    
      <div style={{display:'inline-block', marginLeft:'1.15rem',}}>
-     <img src="dailyplanner.png" style={{width: '8rem', height: '2.25rem'}}/>
+     <img src="dailyplanner.png" style={{width: '12rem', height: '4rem', borderRadius: '0.625rem'}}/>
     
      </div>
      <div style={{display:'inline-block', marginRight:'1.15rem',}}>
-     <img src="goalsaathi_appbar.png" style={{width: '6rem', height: '2.25rem' }}/>
+    
     
      </div>
 
