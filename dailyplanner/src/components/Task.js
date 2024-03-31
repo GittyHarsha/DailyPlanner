6f4301a0df786c0602b97d4dda29af34c622a5db
Harsha Narayana P <hnarayana788@gmail.com>
2024-03-31 17:52:50 +0530

    initial-scale=0.8

--- dailyplanner/src/components/Task.js
+++ dailyplanner/src/components/Task.js
@@ -1,11 +1,22 @@
 import {useSortable} from '@dnd-kit/sortable';
 import {CSS} from '@dnd-kit/utilities/';
-import {Paper} from '@mui/material';
+import {Paper, Tooltip} from '@mui/material';
 import {w, h} from '../services/dimensions.js';
+import {useState} from 'react';
 
 import {Checkbox} from '@mui/material';
 export const Task = ({id, title, handleCheck, isChecked}) => {
+    const [open, setOpen] = useState(false);
 
+    const handleTooltipClose = () => {
+      setOpen(false);
+    
+    };
+  
+    const handleTooltipOpen = () => {
+      setOpen(true);
+    };
+  
     
     const {attributes, listeners, setNodeRef, 
     transform, transition} = useSortable({id});
@@ -16,7 +27,7 @@ export const Task = ({id, title, handleCheck, isChecked}) => {
         marginLeft: '0px',
         marginTop: '5px',
         zIndex: 1,
-        width : `${w(214)}`,
+        width : `${w(210)}`,
         height: `${h(36)}`,
         paddingLeft: '10px',
         display:'flex',
@@ -31,6 +42,7 @@ export const Task = ({id, title, handleCheck, isChecked}) => {
        >
        
         <Paper
+        noWrap
         ref={setNodeRef}
         {...attributes} 
         sx={{my: 1,}}
@@ -43,12 +55,29 @@ export const Task = ({id, title, handleCheck, isChecked}) => {
             onClick={handleCheck}
 
             />
-         <div 
-         style={{ width: `${w(200)}`, textAlign:'left'}
-        
-        } {...listeners}>
-         {title}
-         </div>
+
+                                    <Tooltip 
+                                    open={open}
+                                   
+                                    PopperProps={{
+                                    sx: {
+                                        "& .MuiTooltip-tooltip": {
+                                        color: "black",
+                                        backgroundColor: "white"
+                                        }
+                                    }
+                                    }}
+                                    title={<div>{title}</div>}> 
+                                    <div 
+                                    onMouseEnter={handleTooltipOpen}
+                                    onMouseLeave={handleTooltipClose}
+                                            style={{ width: `${w(200)}`, textAlign:'left', overflow:'clip' }
+                                            
+                                            } {...listeners}>
+                                            {title}
+                                            </div>                                   
+                                     </Tooltip>
+         
         
         </Paper>
         </div>
--- dailyplanner/src/components/TimePicker.js
+++ dailyplanner/src/components/TimePicker.js
@@ -2,34 +2,27 @@ import * as React from 'react';
 
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-
+import {Alert, AlertTitle} from '@mui/material';
 import { DesktopTimePicker } from '@mui/x-date-pickers/DesktopTimePicker';
+import {Button} from '@mui/material';
 import dayjs from 'dayjs';
 
-export default function ResponsiveTimePickers({onChange, default_value, timeThresh}) {
+export default function ResponsiveTimePickers({onChange, default_value, alert, resetAlert}) {
   let today = dayjs();
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
+       {alert && (
+        <Alert severity="warning" sx={{position:'absolute', zIndex:10,}}>
+          <AlertTitle>{alert}</AlertTitle> <Button onClick={resetAlert}>{'X'}</Button>
+        </Alert>
+      )}
   
           <DesktopTimePicker 
          
-          shouldDisableTime={(time, clockType) => {
-            // Disable times before the current time
-            if(timeThresh==null) return false;
           
-            if(timeThresh.isAfter(dayjs()) && timeThresh.format('DD-MM-YYYY')!=dayjs().format('DD-MM-YYYY')) return false;
-            
-            if (clockType === 'hours' && time.isBefore(today, 'hour')) {
-              return true;
-            }
-            if (clockType === 'minutes' && time.isBefore(today, 'minute')) {
-              return true;
-            }
-            return false;
-          }}
 
           label={"Time"}onChange={onChange} defaultValue={default_value} sx={{width: '9vw'}}/>
-       
+      
      
     </LocalizationProvider>
   );
